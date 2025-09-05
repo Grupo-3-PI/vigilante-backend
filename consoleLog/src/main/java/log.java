9bf4c8b3ebaa7448e61b4dae14de8d00ab268e49
luckas-lopes <luckas.silva@sptech.school>
2025-09-05 17:04:38 -0300

    Adicionei cores

--- consoleLog/src/main/java/log.java
+++ consoleLog/src/main/java/log.java
@@ -11,16 +11,21 @@ public class log {
 
         String hora = ((LocalTime.now()).toString()).substring(0, 8);
         String data = (LocalDate.now()).toString().substring(8, 10) + "/" + (LocalDate.now()).toString().substring(5, 7) + "/" + (LocalDate.now()).toString().substring(0, 4);
+        String nome = "";
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        Integer numRandom = ThreadLocalRandom.current().nextInt(1, 3);
+        while (1 > 0) {
 
-        if (numRandom == 1) {
-            System.out.println("✅ Info: Usuário " + nome + " cadastrado com sucesso  [ " + data + " - " + hora + " ]");
-        } else if (numRandom == 2){
-            System.out.println("❌ ERRO: Não foi possível cadastrar o usuário " + nome + "  [ " + data + " - " + hora + " ]");
+            System.out.println("Digite seu nome: ");
+            Integer numRandom = ThreadLocalRandom.current().nextInt(1, 3);
+            nome = scanner.nextLine();
+
+            if (numRandom == 1) {
+                System.out.println("\u001B[32mInfo: Usuário " + nome + " cadastrado com sucesso  [ " + data + " - " + hora + " ]\u001B[0m");
+            } else if (numRandom == 2){
+                System.out.println("\u001B[31mERRO: Não foi possível cadastrar o usuário " + nome + "  [ " + data + " - " + hora + " ]\u001B[0m");
+            }
         }
+
 
     }
 
